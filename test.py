181fd6579ad0faccf3417847be1cc966d3c924e0
Aarush Gupta <git@aarushgupta.com>
2023-06-03 13:29:54 -0700

    bug fixes, periodic table test, and more README

---
+++ test.py
@@ -0,0 +1,31 @@
+import vektor
+import json
+import os.path
+
+import time
+
+source = []
+with open("periodic_table.json") as handler:
+    table = json.load(handler)
+    for element in table["elements"]:
+        source.append(element)
+
+print("importing periodic table")
+start = time.time()
+
+database = vektor.Vektor()
+if os.path.isfile("db.bin"):
+    database.load("db.bin")
+else:
+    database.from_source(source)
+    database.save("db.bin")
+
+end = time.time()
+print(f"time: {(end - start) * 10 ** 3}ms")
+
+start = time.time()
+results = database.query("Lightest element")
+end = time.time()
+print(f"time: {(end - start) * 10 ** 3}ms")
+
+print(results)
--- vektor.py
+++ vektor.py
@@ -1,39 +1,66 @@
 import numpy as np
 import transformers
-import torch # yes, this is just for the type-checking
+import pickle
+
+import torch # yes, this is just for type-checking
 
 tokenizer = transformers.AutoTokenizer.from_pretrained("bert-base-uncased")
 model = transformers.BertModel.from_pretrained("bert-base-uncased")
 
-def embedding(sentence: str) -> torch.Tensor:
-    inputs = tokenizer(sentence, return_tensors = "pt")
+def bert_embedding(sentence: str) -> torch.Tensor:
+    inputs = tokenizer(
+        sentence,
+        return_tensors = "pt",
+        padding = "max_length",
+        max_length = 100,
+        truncation = True
+    )
     outputs = model(**inputs)
-    return outputs.last_hidden_state
+    return outputs.last_hidden_state.detach()
 
-def euclidean(vectors: np.array, query: np.array) -> float:
-    similarities = np.linalg.norm(vectors - query, axis = 1)
-    return 1 / (1 + similarities)
+def euclidean(vectors: np.array, query: np.array) -> list:
+    #distances = np.empty((1, 100, 768), dtype = np.float32)
+    #for i, v in enumerate(vectors):
+        #dist = np.sqrt(np.sum((a - b) ** 2 for a, b in zip(v, query)))
+        #distances = np.vstack((distances, dist))
+    return [np.linalg.norm(query - vec) for vec in vectors]
 
 class Vektor:
     def __init__(
         self,
-        vectors: np.array = np.array([]),
+        vectors: np.array = np.empty((1, 100, 768), dtype = np.float32),
+        embedding: object = bert_embedding,
         similarity: object = euclidean
     ) -> None:
         self.vectors = vectors
-        self.embedding = None
+        self.embedding = embedding
         self.similarity = similarity
-        self.items = []
+        self.references = []
 
     def from_source(self, source: list) -> None:
-        vectors = np.array(self.embedding(source)).astype(np.float32)
-        for v, item in zip(vectors, source):
-            self.vectors = np.vstack([self.vectors, v]).astype(np.float32)
-            self.items.append(item)
+        for ref in source:
+            print("from_source:", self.vectors.shape)
+            vector = np.array(self.embedding(ref["info"]["description"])).astype(np.float32)
+            self.vectors = np.vstack((self.vectors, vector))
+            self.references.append(ref)
 
-    def query(self, vector: np.array, top_k: int = 5) -> list:
-        vector = self.embedding([vector])[0]
+    def save(self, filename: str) -> None:
+        with open(filename, "wb") as handler:
+            pickle.dump({
+                "vectors": self.vectors,
+                "references": self.references
+            }, handler)
+
+    def load(self, filename: str) -> None:
+        with open(filename, "rb") as handler:
+            data = pickle.load(handler)
+        self.vectors = data["vectors"].astype(np.float32)
+        self.references = data["references"]
+
+    def query(self, query: str, top_k: int = 5) -> list:
+        vector = np.array(self.embedding(query))
         similarities = self.similarity(self.vectors, vector)
-        top = np.argsort(similarities, axis = 0)[-top_k:][::-1]
-        return list(zip([self.items[i] for i in top.flatten()], similarities[top].flatten()))
-
+        table = {j: i for i, j in enumerate(similarities)}
+        similarities.sort(reverse = True)
+        top = [self.references[table[i] - 1] for i in similarities][:top_k]
+        return top
