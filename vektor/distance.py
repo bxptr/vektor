f7d248dfe0bd6339b5d3a2d2692b1070d1686f20
Aarush Gupta <git@aarushgupta.com>
2023-06-03 20:17:16 -0700

    took ages but document and explain each section, from start to finish

--- vektor/distance.py
+++ vektor/distance.py
@@ -1,7 +1,19 @@
+"""
+distance algorithms
+"""
+
 import numpy as np
 
 def euclidean(x: np.array, y: np.array) -> np.array:
+    """
+    euclidean distance is quite simple and is the same as taught in algebra 1/2.
+    it finds the literal distance between the two vectors
+    """
     return np.sqrt(np.dot(x - y, x - y))
 
 def cosine(x: np.array, y: np.array) -> np.array:
+    """
+    cosine similarity does not care about the vector's magnitude but just the angle.
+    it always falls between [-1, 1]
+    """
     return 1 - np.dot(x, y) / ((np.dot(x, x) * np.dot(y, y)) ** 0.5)
--- vektor/lsh.py
+++ vektor/lsh.py
@@ -1,12 +1,34 @@
+"""
+this is the LSH (locality sensitive hashing) implementation
+
+it is widely used and at the core of many top tech companies (Google!)
+
+the first approach to come to mind may be brute forcing through the database and
+computing the distance between all vectors to find the best; however, this easily
+increases the time complexity of one search to O(n^2), possibly O(n).
+
+using LSH, even in-memory, can allow us to achieve a sub-linear time complexity and
+limit the search scope.
+"""
+
 import numpy as np
 
 import vektor.distance
 
 def generate_hash(dims: int, vector: np.array) -> str:
+    # compute a random projection of the vector with 32 because we want a 32-bit hash
     projections = np.dot(np.random.randn(32, dims), vector.flatten())
-    return "".join(["1" if i > 0 else "0" for i in projections])
+    return "".join(["1" if i > 0 else "0" for i in projections]) # convert into binary
 
 class MemoryStore:
+    """
+    in-memory store
+
+    although it may seem as simply a dict wrapper, a dict has the goal to
+    minimize collisions, but an LSH wants to maximize collisions and can
+    be imagined as bucketing similar vectors to reduce the search space.
+    """
+
     def __init__(self) -> None:
         self.store = dict()
 
@@ -19,16 +41,25 @@ class MemoryStore:
 class LSH:
     def __init__(self, dims: int, num_tables: int = 1) -> None:
         self.dims = dims
-        self.tables = [MemoryStore() for i in range(num_tables)]
+        self.tables = [MemoryStore() for i in range(num_tables)] # setup tables (for the examples we only need 1)
 
     def index(self, vector: np.array, reference: object) -> None:
         for i, table in enumerate(self.tables):
+            # generate a hash for the vector and store it's reference in all tables for redundancy
             table.append(generate_hash(self.dims, vector), (tuple(vector.tolist()), reference))
 
     def query(self, vector: np.array, top_k: int, distance: object) -> list:
         possible = set()
         for i, table in enumerate(self.tables):
+            # here is the magic, we maximized collisions so each table should point to multiple vectors
+            # we store all of the them into a set to remove duplicates
             possible.update(table.get(generate_hash(self.dims, vector)))
-        ranked = [(i, distance(vector, i)) for i in possible]
-        ranked.sort(key = lambda x: x[1])
-        return ranked[:top_k]
+        ranked = [(i, distance(vector, i)) for i in possible] # compute the distance between input and possible
+        ranked.sort(key = lambda x: x[1]) # sort by distance
+        return ranked[:top_k] # return the top k results
+
+"""
+congrats! you officially have learned how most vector databases work!
+
+for the interested, you can see ./vektor/distance.py for distance functions
+"""
