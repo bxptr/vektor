f752e31273577adfb7ffa0cd400ee0d1936f5795
Aarush Gupta <git@aarushgupta.com>
2023-06-05 16:19:43 -0700

    new generate hash fn (not integrated with LSH yet)

--- vektor/lsh.py
+++ vektor/lsh.py
@@ -1,10 +1,27 @@
 import numpy as np
+import hashlib
+import struct
+import random
 import collections
 import json
 
-def generate_hash(dims: int, vector: np.array) -> str:
-    projections = np.dot(np.random.randn(64, dims), vector.flatten())
-    return "".join(["1" if i > 0 else "0" for i in projections])
+generator = random.Random()
+generator.seed(1337)
+
+mersenne = (1 << 61) - 1
+max_hash = (1 << 32) - 1
+hash_range = (1 << 32)
+
+def generate_hash(n_perms: int, vector: np.array) -> np.array:
+    permutations = np.array([
+        (generator.randint(1, mersenne), generator.randint(0, mersenne))
+        for _ in range(n_perms)
+    ], np.uint64).T
+    sha = haslib.sha1(vector)
+    values = struct.unpack("<I", self.hashobj(sha).digest()[:4])[0]
+    a, b = permutations
+    perm_values = np.bitwise_and((a * values + b) % mersenne, np.uint64(max_hash))
+    return np.minimum(perm_values, np.ones(n_perms, np.uint64) * max_hash)
 
 def integrate(fn: object, a: float, b: float) -> float:
     area = 0.0
@@ -16,19 +33,19 @@ def integrate(fn: object, a: float, b: float) -> float:
 
 class LSH:
     """
-    LSH (locality sensitive hashing) implementation)
+    LSH (locality sensitive hashing) implementation
     """
 
     def __init__(
         self,
         dims: int,
-        threshold: float = 0.9,
-        perms: int = 128,
+        threshold: float = 0,
+        n_perms: int = 128,
         weights = (0.5, 0.5)
     ) -> None:
         self.dims = dims
         self.threshold = threshold
-        self.perms = perms
+        self.n_perms = n_perms
         self.weights = weights
         self.store = dict()
         self.b, self.r = self._optimal()
@@ -38,8 +55,8 @@ class LSH:
     def _optimal(self) -> tuple:
         min_error = float("inf")
         params = (0, 0)
-        for b in range(1, self.perms + 1):
-            max_r = int(self.perms / b)
+        for b in range(1, self.n_perms + 1):
+            max_r = int(self.n_perms / b)
             for r in range(1, max_r + 1):
                 pos_prob_fn = lambda x: 1 - (1 - x ** float(r)) ** float(b)
                 neg_prob_fn = lambda x: 1 - (1 - (1 - x ** float(r)) ** float(b))
@@ -53,7 +70,7 @@ class LSH:
 
     def index(self, vector: np.array, reference: object) -> None:
         reference = json.dumps(reference)
-        self.store[reference] = generate_hash(self.dims, vector)
+        self.store[reference] = [generate_hash(self.dims, vector)]
         for hash_, table in zip(self.store[reference], self.tables):
             table[hash_].append(reference)
 
@@ -61,6 +78,7 @@ class LSH:
         candidates = set()
         for (s, e), table in zip(self.ranges, self.tables):
             hash_ = generate_hash(self.dims, vector)
+            print(hash_)
             print(table.keys())
             if hash_ in table:
                 for key in table[hash_]: candidates.add(key)
