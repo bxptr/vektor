288aa4f84cad40e1035b87d0fa514803744ddb1e
Aarush Gupta <git@aarushgupta.com>
2023-06-06 16:40:27 -0700

    make reference connect more with the actual key

--- examples/lsh.py
+++ examples/lsh.py
@@ -5,8 +5,8 @@ b = vektor.bert_embedding("aarush is smooth")
 c = vektor.bert_embedding("aarush is very cool")
 
 lsh = vektor.lsh.LSH()
-lsh.index(a, "m2")
-lsh.index(b, "m3")
+lsh.index(a, "aarush is very cool")
+lsh.index(b, "aarush is smooth")
 result = lsh.query(c)
 
 print(result)
