7840354b7c19f2b353e19aa31afd67c0aa29684f
Aarush Gupta <git@aarushgupta.com>
2023-06-07 12:29:11 -0700

    distance won't work with vectors, new distance fn for hashes needed

--- vektor/lsh.py
+++ vektor/lsh.py
@@ -73,7 +73,7 @@ class LSH:
         for hash_, table in zip(self.store[reference], self.tables):
             table[hash_].append(reference)
 
-    def query(self, vector: np.array, distance: object, top_k: int) -> None:
+    def query(self, vector: np.array) -> None:
         candidates = set()
         hashed = self._generate_hash(vector)
         for (s, e), table in zip(self.ranges, self.tables):
@@ -81,6 +81,4 @@ class LSH:
             if hash_ in table:
                 for key in table[hash_]:
                     candidates.add(key)
-        ranked = [(i, distance(vector, i)) for i in candidates]
-        ranked.sort(key = lambda x: x[1])
-        return [json.loads(i) for i in ranked[:top_k]]
+        return [json.loads(i) for i in candidates]
